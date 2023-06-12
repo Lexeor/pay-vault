13b5333ea10e4c7f1a0b3e9e9e65f8539988eb7f
Lexeor <lexeor.ross@gmail.com>
2023-06-12 11:26:22 +0400

    NavBar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import MainRoutes from "./Routes";
+import Navbar from "./components/Navbar";
+import { useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+  console.log(location.pathname);
+
   return (
     <div className="App">
+      {location.pathname !== "/login" && <Navbar />}
       <MainRoutes />
     </div>
   );
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <div className="navbar">
+      <span>Navbar</span>
+      <i className="ri-menu-line"></i>
+    </div>
+  );
+};
+
+export default Navbar;
